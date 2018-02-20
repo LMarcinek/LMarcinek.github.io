c85d97cb5a81fa5b847e458c27ba55def5a44180
Lukasz Marcinek <marcinek.lukasz@gmail.com>
2018-02-20 11:37:27 +0100

    start -to-wakeup

--- js/main.js
+++ js/main.js
@@ -1,23 +1,77 @@
-var $headerArrow = '<div id="left-arrow" class="arrow"><i class="fas fa-angle-left "></i></div>'
-+        '<div id="top-arrow" class="arrow"><i class="fas fa-angle-up"></i></div>'+
-        '<div id="right-arrow" class="arrow"><i class="fas fa-angle-right"></i></div>'+
-        '<div id="down-arrow" class="arrow"><i class="fas fa-angle-down"></i></div>'
+$("body").css("overflow", "hidden"); 
 
-
-
-
-
-
-$('#close-side-left').click(function(){
-    $('#side-left').animate({
-        left: '-100%',
-    }, 1000)
-//    $('header').append($headerArrow)
-})
-
-$('#left-arrow').click(function(){
+$('#left-arrow').on('click',function(){
     $('#side-left').animate({
         left:'0',
     }, 1000)
-//    $('.arrow').remove();
+    $('.arrow').hide();
 })
+
+
+$('#side-left').on('click',function(){
+    $('#side-left').animate({
+        left: '-100%',
+    }, 1000)
+    $('.arrow').show()
+})
+
+
+
+$('#top-arrow').on('click',function(){
+    $('#side-top').animate({
+        top:'0',
+    }, 1000)
+    $('.arrow').hide();
+});
+
+$('#side-top').on('click',function(){
+    $('#side-top').animate({
+        top: '-100%',
+    }, 1000)
+    $('.arrow').show()
+})
+
+
+
+$('#right-arrow').on('click',function(){
+    $('#side-right').animate({
+        right:'0',
+    }, 1000)
+    $('.arrow').hide();
+})
+
+
+$('#side-right').on('click',function(){
+    $('#side-right').animate({
+        right: '-100%',
+    }, 1000)
+    $('.arrow').show()
+})
+
+
+
+
+$('#down-arrow').on('click',function(){
+    $('#side-down').animate({
+        top:'0',
+    }, 1000)
+    $('.arrow').hide();
+});
+
+$('#side-down').on('click',function(){
+    $('#side-down').animate({
+        top: '100vh',
+    }, 1000)
+    $('.arrow').show()
+})
+
+
+
+// preloader
+
+ var preloaderEl = document.querySelector('#preloader')
+setTimeout(function(){
+    preloaderEl.classList.add('preloader-hidding')
+    preloaderEl.classList.add('preloader-hiden');
+    preloaderEl.classList.remove('preloader-hidding');
+}, 1000)
